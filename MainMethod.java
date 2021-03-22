2a40fe6b41cd84d6244f792de15e6bf1534c0009
basilayoub <78543993+basilayoub@users.noreply.github.com>
2021-03-23 03:16:23 +0400

    Add files via upload

---
+++ MainMethod.java
@@ -0,0 +1,22 @@
+import Card;
+import Deck;
+import MyList;
+import ListInterface;
+import java.util.Scanner;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Collections;
+import java.util.Random;
+
+public class mainMethod
+
+public static void main(String[] args) {
+
+	Scanner input = new Scanner(System.in);
+	System.out.println("Start game [y]/[n]: ");
+
+    String gameDecision = input.nextLine(); 
+
+
+
+}
